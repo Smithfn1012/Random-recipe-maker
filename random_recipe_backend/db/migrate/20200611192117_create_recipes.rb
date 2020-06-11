2146088b2a440e3c0471cb4a651eb2e5e4f1ad5a
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-11 16:45:16 -0500

    add recipe and image link to recipe table

--- random_recipe_backend/db/migrate/20200611192117_create_recipes.rb
+++ random_recipe_backend/db/migrate/20200611192117_create_recipes.rb
@@ -2,6 +2,8 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
       t.string :title
+      t.string :recipe_link
+      t.string :image_link
 
       t.timestamps
     end
