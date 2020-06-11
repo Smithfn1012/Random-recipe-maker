60a95505c66f69b8da10b6a88da92e0503cc3d0a
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-11 18:17:45 -0500

    getRecipes/recipeAdapter

--- random_recipe_frontend/src/adapters/RecipesAdapter.js
+++ random_recipe_frontend/src/adapters/RecipesAdapter.js
@@ -2,3 +2,7 @@ class RecipesAdapter {
     constructor() {
       this.baseURL = "http://localhost:3000/recipes"
     }
+
+    getRecipes() {
+        return fetch(this.baseURL).then(response => response.json()).then(json => json.data)
+    }
