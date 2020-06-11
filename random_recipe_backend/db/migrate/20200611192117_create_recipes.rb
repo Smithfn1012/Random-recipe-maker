744126af065943af82c303aceadec7c7fbe460ba
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-11 14:57:03 -0500

    uncomment cors.rb

--- random_recipe_backend/db/migrate/20200611192117_create_recipes.rb
+++ random_recipe_backend/db/migrate/20200611192117_create_recipes.rb
@@ -2,8 +2,6 @@ class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
       t.string :title
-      t.string :recipe_link
-      t.string :image_link
 
       t.timestamps
     end
