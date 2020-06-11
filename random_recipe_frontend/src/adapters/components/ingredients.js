58ac41e46b6984d56c31ca296bed2ef31eb4861e
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-11 17:52:39 -0500

    populate ingredient drop down function

--- random_recipe_frontend/src/adapters/components/ingredients.js
+++ random_recipe_frontend/src/adapters/components/ingredients.js
@@ -4,7 +4,18 @@ class Ingredients {
         this.ingredientDropDown = document.getElementById("filter-dropdown");
         this.fetchAndPopulateDropDown();
     }
+
     fetchAndPopulateDropDown() {
         this.adapter.getIngredients().then(json => this.populateIngredientDropDown(json))
+    }
+
+    populateIngredientDropDown(data) {
+        data.sort((a,b) => (a.attributes.name > b.attributes.name) ? 1 : -1)
+        for (let ingredient of data) {
+            let option = document.createElement("option")
+            option.value = ingredient.attributes.name
+            option.innerHTML = ingredient.attributes.name
+            this.ingredientDropDown.appendChild(option)
+        }
     }
 }
