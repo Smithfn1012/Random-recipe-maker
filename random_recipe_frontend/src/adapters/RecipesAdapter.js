0bb7cb4850240efe70062b31c6dbbb518022fadd
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-11 19:10:33 -0500

    add seeds

--- random_recipe_frontend/src/adapters/RecipesAdapter.js
+++ random_recipe_frontend/src/adapters/RecipesAdapter.js
@@ -2,18 +2,18 @@ class RecipesAdapter {
     constructor() {
       this.baseURL = "http://localhost:3000/recipes"
     }
-
+  
     getRecipes() {
-        return fetch(this.baseURL).then(response => response.json()).then(json => json.data)
+      return fetch(this.baseURL).then(response => response.json()).then(json => json.data)
     }
-
+  
     postRecipeToApi(configurationObject) {
-        return fetch(this.baseURL, configurationObject)
-          .then(response => response.json())
-          .catch(error => console.log("Error: " + error))
+      return fetch(this.baseURL, configurationObject)
+        .then(response => response.json())
+        .catch(error => console.log("Error: " + error))
     }
-
+  
     getRecipeByIngredient(ingredient) {
-        return fetch(this.baseURL + `/${ingredient}`).then(response => response.json())
+      return fetch(this.baseURL + `/${ingredient}`).then(response => response.json())
     }
 }
